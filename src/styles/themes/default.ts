ece0a0d2c0f18496eb8c381d5c3ffd6bad25dc81
Maick Souza <maick_a_s@msn.com>
2022-08-27 11:50:13 -0300

    style: setting customization colors #1

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,25 @@
 export const defaultTheme = {
-  white: '#FFF',
+  white: "#FFF",
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+  "gray-100": "#FAFAFA",
+  "gray-200": "#F3F2F2",
+  "gray-300": "#F3F2F2",
+  "gray-400": "#E6E5E5",
+  "gray-500": "#574F4D",
+  "gray-600": "#574F4D",
+  "gray-700": "#574F4D",
+  "gray-800": "#403937",
+  "gray-900": "#272221",
 
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  "green-300": "#00B37E",
+  "green-500": "#00875F",
+  "green-700": "#015F43",
 
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
+  "purple-100": "#EBE5F9",
+  "purple-500": "#8047F8",
+  "purple-800": "#4B2995",
 
-  'yellow-500': '#FBA94C',
-}
+  "yellow-100": "#F1E9C9",
+  "yellow-500": "#DBAC2C",
+  "yellow-600": "#C47F17",
+};
