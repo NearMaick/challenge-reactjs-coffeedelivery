15be31e4ee3095ac811d247f0aecfe677848f34b
Maick Souza <maick_a_s@msn.com>
2022-08-31 13:21:37 -0300

    style: Checkout #2 CheckoutForm

--- src/pages/Checkout/CheckoutForm/index.tsx
+++ src/pages/Checkout/CheckoutForm/index.tsx
@@ -1,9 +1,64 @@
-import { CheckoutFormContainer } from "./styles";
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollarSimple,
+  MapPin,
+  Money,
+} from "phosphor-react";
+import {
+  CheckoutFormContainer,
+  CheckoutFormHeaderContainer,
+  Form,
+} from "./styles";
 
 export function CheckoutForm() {
   return (
     <CheckoutFormContainer>
-      <h1>CheckoutForm</h1>
+      <h2>Complete seu pedido</h2>
+
+      <CheckoutFormHeaderContainer>
+        <div>
+          <MapPin />
+          <span>Endereço de Entrega</span>
+        </div>
+        <p>Informe o endereço onde deseja receber seu pedido:</p>
+      </CheckoutFormHeaderContainer>
+      <Form>
+        <div>
+          <input type='number' placeholder='CEP' />
+          <input type='text' placeholder='Rua' />
+        </div>
+        <input type='number' placeholder='Número' />
+        <input type='text' placeholder='Complemento' />
+        <input type='text' placeholder='Opcional' />
+
+        <input type='number' placeholder='Bairro' />
+        <input type='text' placeholder='Cidade' />
+        <input type='text' placeholder='UF' />
+      </Form>
+      <div>
+        <div>
+          <CurrencyDollarSimple />
+
+          <span>Pagamento</span>
+          <p>
+            O pagamento é feito na entrega. Escolha a forma que deseja pagar
+          </p>
+        </div>
+        <div>
+          <span>
+            <CreditCard /> cartão de crédito
+          </span>
+
+          <span>
+            <Bank /> cartão de débito
+          </span>
+
+          <span>
+            <Money /> dinheiro
+          </span>
+        </div>
+      </div>
     </CheckoutFormContainer>
   );
 }
--- src/pages/Checkout/CheckoutForm/styles.ts
+++ src/pages/Checkout/CheckoutForm/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
 export const CheckoutFormContainer = styled.div``;
+
+export const CheckoutFormHeaderContainer = styled.header``;
+
+export const Form = styled.form`
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
